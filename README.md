485fa99e99b70b46fc5d3f22b865992cbc3ef6cc
iasminesilva <iasmine948@gmail.com>
2024-06-03 14:57:30 -0300

    Automatizando pesquisa do Google

--- README.md
+++
@@ -1,1 +0,0 @@
-Repositório para meus estudos com Selenium..
---
+++ automatizado/src/test/java/automatizado/GoogleTest.java
@@ -0,0 +1,32 @@
+package automatizado;
+
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.postgresql.util.ByteConverter;
+
+import com.testsigma.sdk.WebElement;
+
+public class GoogleTest {
+    private WebDriver driver;
+    private final String URL_BASE = "https://www.google.com/";
+    private final String CAMINHO_DRIVER = "automatizado//resources//chromedriver-v125.0.6422.exe";
+
+    private void iniciar() {
+        System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(URL_BASE);
+        ;
+    }
+
+    @Test
+    public void devePesquisarNoGoogle() {
+        iniciar();
+
+        org.openqa.selenium.WebElement inputPesquisa = driver.findElement(By.name("q"));
+        inputPesquisa.sendKeys("batata frita" + Keys.ENTER);
+
+    }
+}
