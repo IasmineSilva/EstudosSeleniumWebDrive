b8c8688095029b18d07e0da723afcf28e40a43b3
iasminesilva <iasmine948@gmail.com>
2024-06-03 15:08:23 -0300

    fechando o navegador

--- automatizado/src/test/java/automatizado/GoogleTest.java
+++ automatizado/src/test/java/automatizado/GoogleTest.java
@@ -1,6 +1,9 @@
 package automatizado;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -27,6 +30,11 @@ public class GoogleTest {
 
         org.openqa.selenium.WebElement inputPesquisa = driver.findElement(By.name("q"));
         inputPesquisa.sendKeys("batata frita" + Keys.ENTER);
+        driver.findElement(By.id("result-stats")).getText();
+
+        assertTrue(resultado, resultado.contains("aproximadamente"));
+
+        driver.quit();
 
     }
 }
