30901d9fb3b084a25ded3d7ca1f5a171c50cafb1
iasminesilva <iasmine948@gmail.com>
2024-06-04 18:13:04 -0300

    aplicando boas práticas e POO

--- automatizado/src/test/java/automatizado/GoogleTest.java
+++ automatizado/src/test/java/automatizado/GoogleTest.java
@@ -6,27 +6,11 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.postgresql.util.ByteConverter;
 
-import com.testsigma.sdk.WebElement;
-
-public class GoogleTest {
-    private WebDriver driver;
-    private final String URL_BASE = "https://www.google.com/";
-    private final String CAMINHO_DRIVER = "automatizado//resources//chromedriver-v125.0.6422.exe";
-
-    private void iniciar() {
-        System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(URL_BASE);
-        ;
-    }
+public class GoogleTest extends BaseTest {
 
     @Test
     public void devePesquisarNoGoogle() {
-        iniciar();
 
         org.openqa.selenium.WebElement inputPesquisa = driver.findElement(By.name("q"));
         inputPesquisa.sendKeys("batata frita" + Keys.ENTER);
