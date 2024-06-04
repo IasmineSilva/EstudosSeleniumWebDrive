0c1348b045cc18b62e69d4017cae406b7b75d6c4
iasminesilva <iasmine948@gmail.com>
2024-06-04 19:28:57 -0300

    instanciando classes

--- automatizado/pageObject/GooglePO.java
+++ automatizado/pageObject/GooglePO.java
@@ -3,8 +3,13 @@ package automatizado.page;
 import org.openqa.selenium.WebDriver;
 
 public class GooglePO extends BasePO{
+
     @FindBy(name = "q")
     public WebElement inputPesquisa;
+
+    @FindBy(id = "results-stats")
+    public WebElement divResultadoPesquisa;
+
     @param driver
     protected GooglePO(WebDriver driver){
         super(driver);
